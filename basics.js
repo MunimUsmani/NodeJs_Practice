22ee1b35eb70b8c566f8bbbc0a1542599d458630
MunimUsmani <abdulmunimusmani152@gmail.com>
2025-03-19 00:54:24 +0500

    practicing javascript basics before the main concepts of node js
    contains topics like variables functions arrays loops objects conditional statements

---
+++ basics.js
@@ -0,0 +1,52 @@
+// variables and data types
+// let a = 5;
+// let b = 10;
+// ans = a + b;
+
+
+// console.log(ans);
+// console.log("I am a software engineer");
+
+//array
+// const cars = ['toyota', 'honda', 'bmw', 'benz'];
+
+// cars.push('audi');
+// console.log(cars[2]);
+// console.log(cars);
+
+//if else and forloop
+// let hour = 16;
+
+// if (hour < 18) {
+//     console.log("Good day");
+// } else {
+//     console.log("Good evening");
+// }
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+//objects
+// const person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };  
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.isMarried);
+
+//functions
+// const age = [16, 40, 44, 19];
+// const result = age.filter(checkAdult);
+
+
+// function checkAdult(age) {
+//     return age >= 18;
+// }
+
+// console.log(result);
